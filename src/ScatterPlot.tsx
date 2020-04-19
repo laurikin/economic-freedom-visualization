3a2b6aca9d0b80b43d725400d70c85af4326010f
Lauri Kinnunen <lauri@vividbytes.io>
2020-04-19 13:23:29 +0300

    make points bigger

--- src/ScatterPlot.tsx
+++ src/ScatterPlot.tsx
@@ -107,7 +107,7 @@ export const ScatterPlot = ({ data, xDomain, yDomain }: IScatterPlotProps) => {
                             <foreignObject
                                 width="100"
                                 height="50"
-                                x={labelLeft ? -113 : 13}
+                                x={labelLeft ? -115 : 15}
                                 y="-25"
                             >
                                 <div
